1e655f326da35084c21be39f58e9fd75ed0dfc6f
Yoliani <edrayoca@outlook.es>
2021-12-18 13:24:41 -0500

    commit

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -2,43 +2,31 @@ const { response } = require('express');
 const jwt = require('jsonwebtoken');
 
 const validarJWT = (req, res = response, next) => {
+  //x-token headers
+  const token = req.header('x-token');
 
-    //x-token headers
-    const token = req.header('x-token');
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: 'No hay token en la peticion',
+    });
+  }
 
-    if (!token) {
-        return res.status(401).json({ 
-            ok: false,
-            msg: 'No hay token en la peticion'
-        });
-    }
+  try {
+    const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
 
-    try {
+    req.uid = uid;
+    req.name = name;
+  } catch (error) {
+    return res.status(401).json({
+      ok: false,
+      msg: 'Token no valido',
+    });
+  }
 
-        const { uid, name} = jwt.verify(
-            token,
-            process.env.SECRET_JWT_SEED,
-            );
-
-            req.uid = uid;
-            req.name = name;
-        
-
-    } catch (error) {
-        return res.status(401).json({
-            ok:false,
-            msg: 'Token no valido'
-        });
-    }
-    
-
-    
-
-
-    next();
-}
-
+  next();
+};
 
 module.exports = {
-    validarJWT
-}
+  validarJWT,
+};
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,20 +1,34 @@
 const { Schema, model } = require('mongoose');
 
+const validator = require('validator');
 const UsuarioSchema = Schema({
   name: {
     type: String,
     require: true,
+    validate: (value) => {
+      const regexp =
+        /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
+
+      return regexp.test(value);
+    },
   },
 
   lastname: {
     type: String,
     require: true,
+    validate: (value) => {
+      const regexp =
+        /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
+
+      return regexp.test(value);
+    },
   },
 
   email: {
     type: String,
     require: true,
     unique: true,
+    validate: (value) => validator.isEmail(value),
   },
   password: {
     type: String,
